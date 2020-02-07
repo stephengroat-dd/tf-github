4114fdedc26562516aa7e3917d26c4d0d9809d65
Stephen Groat <stephen.groat@datadoghq.com>
2020-02-07 08:35:42 -0800

    data team block

--- github_team/main.tf
+++ github_team/main.tf
@@ -21,24 +21,25 @@ variable user_info {
 resource "github_membership" "team-member" {
   for_each = var.user_info
   username = each.key
-  role = "member"
+  role = "admin"
 }
 
 # TODO see if a data block can be used instead of a resource block to avoid team creation
-resource "github_team" "team" {
+data "github_team" "team" {
   for_each = var.teams
-  name = each.value
+  slug = each.value
 }
 
 resource "github_team_membership" "team-member" {
   for_each = local.users_list
   username = split(",", each.value)[0]
   team_id  = lookup(local.team_list, split(",", each.value)[1], "none")
+  role = "maintainer"
 }
 
 locals { 
   # map of team to team ID
-  team_list = {for teamname in var.teams: teamname => github_team.team[teamname].id}
+  team_list = {for teamname in var.teams: teamname => data.github_team.team[teamname].id}
   # set of strings of users and any associated teams in CSV format, duplicating entries for users with multiple team membership
   users_list = toset(flatten([
     for user, teams in var.user_info : [
